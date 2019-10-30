512a7f10ccd35eb7f86aaf666330fb42f986895d
yuri0818 <yyyy.0818.0830@gmail.com>
2019-10-30 18:47:07 +0000

    Finish editing-users

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+# coding: utf-8
+
+User.create!(name: "Sample User",
+             email: "sample@email.com",
+             password: "password",
+             password_confirmation: "password",
+             admin: true)
+
+60.times do |n|
+  name  = Faker::Name.name
+  email = "sample-#{n+1}@email.com"
+  password = "password"
+  User.create!(name: name,
+               email: email,
+               password: password,
+               password_confirmation: password)
+end
